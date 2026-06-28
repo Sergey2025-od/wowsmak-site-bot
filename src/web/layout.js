bbfba15ad496a59535d4783825092103ffbbe3f1
Sergey2025-od <2017obzor2017@gmail.com>
2026-06-28 22:37:41 +0300

    Update layout.js

--- src/web/layout.js
+++ src/web/layout.js
@@ -141,7 +141,7 @@ export function layout(content, opts = {}) {
   <link rel="icon" href="/assets/img/favicon.svg" type="image/svg+xml" />
   <link rel="alternate icon" href="/assets/img/favicon-32.png" sizes="32x32" type="image/png" />
   <link rel="apple-touch-icon" href="/assets/img/apple-touch-icon.png" />
-  <link rel="stylesheet" href="/assets/css/site.css?v=22" />${analyticsHead()}
+  <link rel="stylesheet" href="/assets/css/site.css?v=25" />${analyticsHead()}
   ${allJsonLd}
 </head>
 <body class="${esc(bodyClass)}">
